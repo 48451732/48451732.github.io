643170df3dea2d66b11b39140afb1df058e04545
48451732 <81823026+48451732@users.noreply.github.com>
2021-05-07 11:21:58 -0500

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+#variable de tipo cadena
+nombre = "Jennifer"
+ape = 'Rudas'
+nomape = nombre + ' ' + ape
+#imprimir cadenas
+print(nombre)
+print(ape)
+print(nomape)
+#visualizar tipo de dato
+print(type(nombre))
+print(type(ape))
+print(type(nomape))
